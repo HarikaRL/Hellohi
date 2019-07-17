18ac92c6f8826992f9fb2f5eb743756aa968ba88
SandraV1 <sandravishnu24@gmail.com>
2019-07-17 17:35:50 -0400

    stuffnew

---
+++ app/src/main/java/com/example/hellohi/SignUp.java
@@ -0,0 +1,17 @@
+package com.example.hellohi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //sandras comment
+    }
+    //commentbyHarika
+}//commentsandra
