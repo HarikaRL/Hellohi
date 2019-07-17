7cc04eabce5510db42411bfbc736c4d09f8b8b06
HarikaRL <harika.lingamarla@gmail.com>
2019-07-17 17:28:15 -0400

    Stuffy

---
+++ app/src/main/java/com/example/hellohi/CreateLibrary.java
@@ -0,0 +1,14 @@
+package com.example.hellohi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateLibrary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_library);
+    }
+}
---
+++ app/src/main/java/com/example/hellohi/ReturnItem.java
@@ -0,0 +1,14 @@
+package com.example.hellohi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReturnItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_return_item);
+    }
+}
